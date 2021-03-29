9f78146fada2beccd5f14d7e82aaa09983ad2482
jinwooPark6 <jinwoo13245@naver.com>
2021-03-29 23:30:23 +0900

    0329

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 
 public interface MemberRepository {
 
+    @Repository
+    public class MemoryMemberRepository implements MemberRepository {}
+
+
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
