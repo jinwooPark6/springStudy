3a96d09c93c4e7d52ee5ff62b5d56df9e9c1036a
jinwooPark6 <jinwoo13245@naver.com>
2021-03-30 19:57:17 +0900

    0330

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,15 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-
-    @Repository
-    public class MemoryMemberRepository implements MemberRepository {}
-
 
     Member save(Member member);
     Optional<Member> findById(Long id);
